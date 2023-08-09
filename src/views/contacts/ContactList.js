5aff4a46656deb9bb741e201ae88812a341f4f4a
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-08-09 17:49:19 +0530

    show data in table

--- src/views/contacts/ContactList.js
+++ src/views/contacts/ContactList.js
@@ -1,4 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import {
+  CTable,
+  CTableHead,
+  CTableRow,
+  CTableHeaderCell,
+  CTableBody,
+  CTableDataCell,
+} from '@coreui/react'
 
 const ContactList = () => {
   const [contacts, setContacts] = useState([])
@@ -11,9 +19,38 @@ const ContactList = () => {
     const response = await fetch('http://localhost:8000/contacts')
     const resData = await response.json()
     console.log(resData)
+    setContacts(resData)
   }
 
-  return <div>ContactList</div>
+  return (
+    <>
+      <CTable>
+        <CTableHead>
+          <CTableRow>
+            <CTableHeaderCell scope="col">#</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Email</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Phone</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Message</CTableHeaderCell>
+          </CTableRow>
+        </CTableHead>
+
+        <CTableBody>
+          {contacts.map((contact) => {
+            return (
+              <CTableRow key={contact.id}>
+                <CTableHeaderCell scope="row">{contact.id}</CTableHeaderCell>
+                <CTableDataCell>{contact.name}</CTableDataCell>
+                <CTableDataCell>{contact.email}</CTableDataCell>
+                <CTableDataCell>{contact.phone}</CTableDataCell>
+                <CTableDataCell>{contact.message}</CTableDataCell>
+              </CTableRow>
+            )
+          })}
+        </CTableBody>
+      </CTable>
+    </>
+  )
 }
 
 export default ContactList
