6bcff884475962768d0ef53700f5e3c5df54a97c
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-08-09 16:39:20 +0530

    fetch data from server

--- src/views/contacts/ContactList.js
+++ src/views/contacts/ContactList.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const ContactList = () => {
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    loadContactsFromServer()
+  }, [])
+
+  const loadContactsFromServer = async () => {
+    const response = await fetch('http://localhost:8000/contacts')
+    const resData = await response.json()
+    console.log(resData)
+  }
+
   return <div>ContactList</div>
 }
 
